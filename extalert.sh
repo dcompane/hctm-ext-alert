1a47ca3a09479fffdc1367a806b9a783b211ccea
dcompane <dcompane@gmail.com>
2022-12-23 15:36:15 -0800

    After many changes

---
+++ extalert.sh
@@ -0,0 +1,5 @@
+# /bin/bash
+
+set -v 
+
+$(which python) /home/saasaapi/extalerts/extalert.py $*
--- setalerts.sh
+++ setalerts.sh
@@ -1,5 +1,10 @@
-ctm run alerts::close true
-ctm run alerts:stream:temnplat::set -f field_names.json
+#! /bin/bash
+set -x
+
+ctm run alerts:listener:script::set /home/saasaapi/extalerts/extalert.sh
 ctm run alerts:listener:environment::set $(ctm env show |grep -i current |awk '{print $3}')
-ctm run alerts::status
-ctm run alerts::open
+ctm run alerts:stream:template::set -f field_names.json
+ctm run alerts:stream::status
+ctm run alerts:stream::open
+ctm run alerts:stream::status
+ctm run alerts:listener::start
